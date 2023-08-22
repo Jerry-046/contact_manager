ce1f39d05e24fa5046d2f04ba8d4852032cdedcf
Jerry <dare3462@gmail.com>
2023-08-22 12:42:56 +0545

    Commit after Course Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Contact from "./component/Contact";
+import ContactAdder from "./component/contactAdder";
+import "./styles/app.css";
+import Navbar from "./component/navbar";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
-function App() {
+const App = () => {
+  const getContact = JSON.parse(localStorage.getItem("contact"));
+  const [contact, setcontact] = useState(getContact ? getContact : []);
+
+  const addContactdata = (contactdata) => {
+    const allContact = [contactdata, ...contact];
+    setcontact(allContact);
+    localStorage.setItem("contact", JSON.stringify(allContact));
+  };
+
+  const cleardata = () => {
+    confirmAlert({
+      title: "Confirm to submit",
+      message: "Are you sure to do this.",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => {
+            setcontact([]);
+            localStorage.clear();
+          },
+        },
+        {
+          label: "No",
+          // onClick: () => alert("Click No")
+        },
+      ],
+    });
+    // };
+  };
+
+  const childFunctionHandler = () => {
+    alert("Grand Parents was Called");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="contact_adder">
+        <ContactAdder
+          onContactAdded={addContactdata}
+          childfunction={childFunctionHandler}
+        />
+      </div>
+      <hr />
+      <div className="contact_list">
+        Contact List
+        <hr />
+        {/* <Contact data={contact[0]} />
+        <Contact data={contact[1]} />
+        <Contact data={contact[2]} /> */}
+        {contact.map((data) => (
+          <Contact key={data.id} data={data}></Contact>
+        ))}
+      </div>
+      <button type="button" onClick={cleardata}>
+        Clear All Contact
+      </button>
+    </>
   );
-}
+};
 
 export default App;
